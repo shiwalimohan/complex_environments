2320ae61412077f52ea594e4993490b7a704bd55
Shiwali Mohan <shiwali.mohan@gmail.com>
2014-01-10 10:20:19 -0500

    changed the angle of the gripper

--- controller.py
+++ controller.py
@@ -4,38 +4,17 @@ import time
 def send_pose(s, morse, x, y, yaw):
     s.publish({'x' : x, 'y' : y, 'z' : 0.0, \
                'yaw' : yaw, 'pitch' : 0.0, 'roll' : 0.0})
-    time.sleep(1)
+    time.sleep(0.1)
 
 with pymorse.Morse("localhost", 4000) as simu:
 
+    # teleport_stream = simu.rosie.teleport
 
+    # send_pose(teleport_stream, simu, -1.0, 0, 0.0)
+    time.sleep(0.1)
 
-    teleport_stream = simu.rosie.teleport
-    kuka_client = simu.rosie.arm
+    obj = simu.rosie.torso.r_arm.gripper.release()
+    print(obj)
 
-    kuka_client.set_rotation('kuka_2', -1.57)
-    time.sleep(1) 
-
-    send_pose(teleport_stream, simu, 1.0, 5, 0.0)
     time.sleep(1)
-
-    obj = simu.rosie.arm.gripper.grab()
-
-    print(obj)
-
-    send_pose(teleport_stream, simu, -2.0, 0, 0.0)
-    time.sleep(1)
-
-    obj = simu.rosie.arm.gripper.release()
-
-    print(obj)
-
-    obj = simu.rosie.arm.gripper.grab()
-
-    print(obj)
-
-    obj = simu.rosie.arm.gripper.release()
-
-    print(obj)
-
-
+        
